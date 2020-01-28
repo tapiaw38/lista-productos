8f990760f374fc355d836c022b6afb8bb65ee28f
tapiaw38 <tapiaw38@gmail.com>
2020-01-28 15:22:14 +0100

    update2

--- app.py
+++ app.py
@@ -1,63 +1,144 @@
-from tkinter import *
+
 from tkinter import ttk
+from tkinter import *
+
 import sqlite3
 
 class Product:
-    db_name='database.db'
+    # connection dir property
+    db_name = 'database.db'
 
     def __init__(self, window):
-        self.root=window
-        self.root.title('Products Application')
-         #creating frame
-        frame=LabelFrame(self.root, text='Registra un nuevo producto')
-        frame.grid(row=0,column=0, columnspan=3,pady=20)
-        #input
-        Label(frame,text='Nombre: ').grid(row=1, column=0)
+        # Initializations
+        self.wind = window
+        self.wind.title('Productos Application')
+
+        # Creating a Frame Container
+        frame = LabelFrame(self.wind, text = 'Registrar nuevo Producto')
+        frame.grid(row = 0, column = 0, columnspan = 3, pady = 20)
+
+        # Name Input
+        Label(frame, text = 'Nombre: ').grid(row = 1, column = 0)
         self.name = Entry(frame)
         self.name.focus()
-        self.name.grid(row=1,column=3)
-        #--
-        Label(frame,text='Precio: ').grid(row=2, column=0)
-        self.price = Entry(frame)
-        self.price.grid(row=2,column=3)
-        #button
-        ttk.Button(frame,text='Guardar Producto').grid(row=3,columnspan=2,sticky= W + E)
-        #table
-        self.tree=ttk.Treeview(height=10,columns=2)
-        self.tree.grid(row=4,column=0, columnspan=2)
-        self.tree.heading('#0', text='Nombre',anchor=CENTER)
-        self.tree.heading('#1', text='Precio',anchor=CENTER)
+        self.name.grid(row = 1, column = 1)
 
+        # Price Input
+        Label(frame, text = 'Precio: ').grid(row = 2, column = 0)
+        self.price = Entry(frame)
+        self.price.grid(row = 2, column = 1)
+
+        # Button Add Product
+        ttk.Button(frame, text = 'Guardar Producto', command = self.add_product).grid(row = 3, columnspan = 2, sticky = W + E)
+
+        # Output Messages
+        self.message = Label(text = '', fg = 'red')
+        self.message.grid(row = 3, column = 0, columnspan = 2, sticky = W + E)
+
+        # Table
+        self.tree = ttk.Treeview(height = 10, columns = 2)
+        self.tree.grid(row = 4, column = 0, columnspan = 2)
+        self.tree.heading('#0', text = 'Nombre', anchor = CENTER)
+        self.tree.heading('#1', text = 'Precio', anchor = CENTER)
+
+        # Buttons
+        ttk.Button(text = 'BORRAR', command = self.delete_product).grid(row = 5, column = 0, sticky = W + E)
+        ttk.Button(text = 'EDITAR', command = self.edit_product).grid(row = 5, column = 1, sticky = W + E)
+
+        # Filling the Rows
+        #Agregar datos a los campos
         self.get_products()
 
-    #iniciar consulta
-    def run_query(self,query,parameters=()):
+    # Function to Execute Database Querys
+    def run_query(self, query, parameters = ()):
         with sqlite3.connect(self.db_name) as conn:
-            cursor =conn.cursor()
-            result=cursor.execute(query,parameters)
+            cursor = conn.cursor()
+            result = cursor.execute(query, parameters)
             conn.commit()
         return result
-    #consulta trae datos de productos
+
+    # Get Products from Database
     def get_products(self):
-        #obtener todos los datos de la tabla para limpiarlos
-        records=self.tree.get_children()
+        # cleaning Table
+        records = self.tree.get_children()
         for element in records:
             self.tree.delete(element)
-        query='SELECT * FROM productos ORDER BY nombre DESC'
-        db_rows=self.run_query(query)
+        # getting data
+        query = 'SELECT * FROM product ORDER BY name DESC'
+        db_rows = self.run_query(query)
+        # filling data
         for row in db_rows:
-            self.tree.insert('',0,text=row[1],values=row[2])
+            self.tree.insert('', 0, text = row[1], values = row[2])
+
+    # User Input Validation
     def validation(self):
         return len(self.name.get()) != 0 and len(self.price.get()) != 0
+
     def add_product(self):
         if self.validation():
-            pass
+            query = 'INSERT INTO product VALUES(NULL, ?, ?)'
+            parameters =  (self.name.get(), self.price.get())
+            self.run_query(query, parameters)
+            self.message['text'] = 'Producto {} guardado correctamente'.format(self.name.get())
+            self.name.delete(0, END)
+            self.price.delete(0, END)
         else:
-            pass
+            self.message['text'] = 'Nombre y Precio son Requiridos'
+        self.get_products()
 
+    def delete_product(self):
+        self.message['text'] = ''
+        try:
+           self.tree.item(self.tree.selection())['text'][0]
+        except IndexError as e:
+            self.message['text'] = 'Por favor seleccione un Registro'
+            return
+        self.message['text'] = ''
+        name = self.tree.item(self.tree.selection())['text']
+        query = 'DELETE FROM product WHERE name = ?'
+        self.run_query(query, (name, ))
+        self.message['text'] = 'Registro {} borrado correctamente'.format(name)
+        self.get_products()
 
-if __name__=='__main__':
-    window=Tk()
+    def edit_product(self):
+        self.message['text'] = ''
+        try:
+            self.tree.item(self.tree.selection())['values'][0]
+        except IndexError as e:
+            self.message['text'] = 'Por favor, seleccione un registro'
+            return
+        name = self.tree.item(self.tree.selection())['text']
+        old_price = self.tree.item(self.tree.selection())['values'][0]
+        self.edit_wind = Toplevel()
+        self.edit_wind.title = 'Edit Product'
+        # Old Name
+        Label(self.edit_wind, text = 'Nombre:').grid(row = 0, column = 1)
+        Entry(self.edit_wind, textvariable = StringVar(self.edit_wind, value = name), state = 'readonly').grid(row = 0, column = 2)
+        # New Name
+        Label(self.edit_wind, text = 'Nuevo Nombre:').grid(row = 1, column = 1)
+        new_name = Entry(self.edit_wind)
+        new_name.grid(row = 1, column = 2)
+
+        # Old Price
+        Label(self.edit_wind, text = 'Precio:').grid(row = 2, column = 1)
+        Entry(self.edit_wind, textvariable = StringVar(self.edit_wind, value = old_price), state = 'readonly').grid(row = 2, column = 2)
+        # New Price
+        Label(self.edit_wind, text = 'Nuevo Precio:').grid(row = 3, column = 1)
+        new_price= Entry(self.edit_wind)
+        new_price.grid(row = 3, column = 2)
+
+        Button(self.edit_wind, text = 'Modificar', command = lambda: self.edit_records(new_name.get(), name, new_price.get(), old_price)).grid(row = 4, column = 2, sticky = W)
+        self.edit_wind.mainloop()
+
+    def edit_records(self, new_name, name, new_price, old_price):
+        query = 'UPDATE product SET name = ?, price = ? WHERE name = ? AND price = ?'
+        parameters = (new_name, new_price,name, old_price)
+        self.run_query(query, parameters)
+        self.edit_wind.destroy()
+        self.message['text'] = 'Registro {} modificado correctamente'.format(name)
+        self.get_products()
+
+if __name__ == '__main__':
+    window = Tk()
     application = Product(window)
-    if __name__ == '__main__':
-        window.mainloop()
+    window.mainloop()
